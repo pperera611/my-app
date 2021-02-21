7ea6f8f35e511648dd3401ec7e027d0efac7b4c4
pperera611 <pperera611@gmail.com>
2021-02-21 11:01:26 -0300

    myApp version final

---
+++ src/main/java/com/Pablo/myapp/entity/TestEntity.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.Pablo.myapp.entity;
+
+/**
+ * @author Pablo Perera
+ *
+ */
+public class TestEntity {
+
+	
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
